4355cf12308bf78e649bfe3d1e1e576158baab6e
icsces <77221447+icsces@users.noreply.github.com>
2023-06-07 09:28:14 +0900

    extend

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,7 @@
     /* ==========================================================================
        countdown timer
        ========================================================================== */
-     jQuery('#clock').countdown('2023/06/07',function(event){
+     jQuery('#clock').countdown('2023/06/21',function(event){
       var $this=jQuery(this).html(event.strftime(''
       +'<div class="time-entry days" style="background-color:#48bfe3"><span>%-D</span> Days</div> '
       +'<div class="time-entry hours"style="background-color:#56cfe1"><span>%H</span> Hours</div> '
